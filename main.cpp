c06dd2bf70b8739731642cc816040b0a6f7b33a2
DanielaZaragoza <167575653+DanielaZaragoza@users.noreply.github.com>
2024-05-26 20:16:07 -0600

    Update main.cpp
    se incluyó la función ctime

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include "Grafo.h"
+#include <ctime>  // Incluimos la biblioteca ctime
 
 using namespace std;
 
@@ -22,13 +23,19 @@ int main() {
     cout << " " << endl;
 
     cout << "----------Busqueda en Anchura (BFS)----------" << endl;
+    clock_t start_bfs = clock();  // Start time for BFS
     g.busquedaAnchura();
+    clock_t end_bfs = clock();  // End time for BFS
+    double elapsed_bfs = double(end_bfs - start_bfs) / CLOCKS_PER_SEC * 1000;
+    cout << "Tiempo de ejecución BFS: " << elapsed_bfs << " ms" << endl;
     cout << " " << endl;
 
     cout << "----------Busqueda en Profundidad (DFS)----------" << endl;
+    clock_t start_dfs = clock();  // Start time for DFS
     g.busquedaProfundidad();
-
-
+    clock_t end_dfs = clock();  // End time for DFS
+    double elapsed_dfs = double(end_dfs - start_dfs) / CLOCKS_PER_SEC * 1000;
+    cout << "Tiempo de ejecución DFS: " << elapsed_dfs << " ms" << endl;
 
     return 0;
 }
