2a32ffac0e7dd61b8a30b0cad60cf01938129ae5
Adan Zamora <108899515+Vigilanti182@users.noreply.github.com>
2024-05-28 11:32:36 -0600

    Actualización menu ADAN
    Corrección de errores en la ejecución del menu

--- main.cpp
+++ main.cpp
@@ -1,70 +1,88 @@
 #include <iostream>
 #include <fstream>
 #include "Grafo.h"
-#include <ctime>
-
+#include <ctime> // Incluimos esta biblioteca para cronometrar el tiempo de ejecución
 using namespace std;
+
+void mostrarMenu() {
+    cout << "------ MENU ------" << endl;
+    cout << "1. Mostrar Matriz de Adyacencia Vacia" << endl;
+    cout << "2. Mostrar Lista de Nodos" << endl;
+    cout << "3. Generar y Mostrar Matriz de Adyacencia del Grafo" << endl;
+    cout << "4. Realizar Busqueda en Anchura (BFS)" << endl;
+    cout << "5. Realizar Busqueda en Profundidad (DFS)" << endl;
+    cout << "6. Salir" << endl;
+    cout << "Elija una opcion: ";
+}
 
 int main() {
     string s = "Laberinto.txt";
     Grafo g(s);
     int opcion;
-
     do {
-        cout << "Menú:" << endl;
-        cout << "1. Mostrar matriz de adyacencia vacía" << endl;
-        cout << "2. Generar y mostrar lista de nodos" << endl;
-        cout << "3. Generar matriz de adyacencia del grafo" << endl;
-        cout << "4. Realizar búsqueda en anchura (BFS)" << endl;
-        cout << "5. Realizar búsqueda en profundidad (DFS)" << endl;
-        cout << "0. Salir" << endl;
-        cout << "Ingrese una opción: ";
+        mostrarMenu();
         cin >> opcion;
-
         switch (opcion) {
             case 1:
+
                 cout << "----------MATRIZ de Adyacencia Vacia----------" << endl;
                 g.muestraMatriz();
-                cout << endl;
+                cout << " " << endl;
                 break;
+
             case 2:
+
                 cout << "----------Lista de Nodos----------" << endl;
                 g.generaListaNodos();
                 g.muestraListaNodos();
-                cout << endl;
+                cout << " " << endl;
+
                 break;
             case 3:
+
                 cout << "----------Matriz de Adyacencia del Grafo----------" << endl;
                 g.generaMatriz();
-                cout << endl;
-                break;
-            case 4:
-                cout << "----------Busqueda en Anchura (BFS)----------" << endl;
-                clock_t start_bfs = clock();
-                g.busquedaAnchura();
-                clock_t end_bfs = clock();
-                double elapsed_bfs = double(end_bfs - start_bfs) / CLOCKS_PER_SEC * 1000;
-                cout << "Tiempo de ejecución BFS: " << elapsed_bfs << " ms" << endl;
-                cout << endl;
-                break;
-            case 5:
-                cout << "----------Busqueda en Profundidad (DFS)----------" << endl;
-                clock_t start_dfs = clock();
-                g.busquedaProfundidad();
-                clock_t end_dfs = clock();
-                double elapsed_dfs = double(end_dfs - start_dfs) / CLOCKS_PER_SEC * 1000;
-                cout << "Tiempo de ejecución DFS: " << elapsed_dfs << " ms" << endl;
-                cout << endl;
-                break;
-            case 0:
-                cout << "Saliendo del programa..." << endl;
-                break;
-            default:
-                cout << "Opción inválida. Por favor, ingrese una opción válida." << endl;
-                cout << endl;
-                break;
-        }
-    } while (opcion != 0);
+                g.muestraMatriz();
+                cout << " " << endl;
 
+                break;
+            case 4: {
+
+                cout << "----------Busqueda en Anchura (BFS)----------" << endl;
+                g.generaMatriz(); // Se tiene que mandar llamar al metodo genera matriz antes para
+                //para que la busqueda BFS se ejecute correctamente
+
+                clock_t start_bfs = clock(); // Incializar tiempo para BFS
+                g.busquedaAnchura();
+
+                clock_t end_bfs = clock(); // Finalizar tiempo para BFS
+                double elapsed_bfs = double(end_bfs - start_bfs) / CLOCKS_PER_SEC * 1000;
+                cout << "Tiempo de ejecucion BFS: " << elapsed_bfs << " ms" << endl;
+                cout << " " << endl;
+                break;
+            }
+            case 5: {
+
+                cout << "----------Busqueda en Profundidad (DFS)----------" << endl;
+                g.generaMatriz();
+
+                clock_t start_dfs = clock(); // inicializar tiempo para DFS
+                g.busquedaProfundidad();
+
+                clock_t end_dfs = clock(); // finalizar tiempo para DFS
+                double elapsed_dfs = double(end_dfs - start_dfs) / CLOCKS_PER_SEC * 1000;
+                cout << "Tiempo de ejecucion DFS: " << elapsed_dfs << " ms" << endl;
+                cout << " " << endl;
+                break;
+            }
+            case 6:
+
+                cout << "Saliendo del programa." << endl;
+                break;
+
+            default:
+                cout << "Opcion no valida. Por favor, intente de nuevo." << endl;
+        }
+    } while (opcion != 6);
     return 0;
 }
